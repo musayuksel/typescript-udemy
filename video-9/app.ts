e65de007705ce005220c9909accd87601fb9ee2b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-25 17:01:36 +0100

    Add type and refactor the code

--- video-9/app.ts
+++ video-9/app.ts
@@ -1,8 +1,8 @@
-function add(num1, num2) {
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.8;
 
 const result = add(number1, number2);
