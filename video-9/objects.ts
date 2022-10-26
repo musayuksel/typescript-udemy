848e5721478f05ebc175ae37f705e29eb4756606
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-26 09:21:38 +0100

    Add arrays

--- video-9/objects.ts
+++ video-9/objects.ts
@@ -1,14 +1,23 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  name: 'Max',
-  age: 30,
-};
-console.log(person.name);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'Max',
+//   age: 30,
+// };
+// console.log(person.name);
 
 // Better syntax for the above:
 const person2 = {
   name: 'Max',
   age: 30,
+  hobbies: ['Sports', 'Cooking'], // Array of strings
 };
+
+// let favoriteActivities: string[]; // Array of strings
+// favoriteActivities = ['Sports'];
+
+person2.hobbies.forEach((hobby) => {
+  console.log(hobby.toUpperCase()); //ts knows that hobby is a string
+  // console.log(hobby.map()); // !!! ERROR !!!
+});
