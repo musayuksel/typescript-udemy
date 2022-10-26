04327484f0b0d21023d910a52d470b991ff4040e
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-26 15:13:24 +0100

    Add neverAndUnknown types

---
+++ video-9/unknownAndNeverTypes.ts
@@ -0,0 +1,14 @@
+let userUnknownInput: unknown;
+let userKnownInput: string | number;
+
+userUnknownInput = 'Max';
+userUnknownInput = 5;
+userKnownInput = 'Musa';
+//userKnownInput = userUnknownInput //error because we don't know what type is userUnknownInput
+
+//NEVER TYPE
+function generateError(message: string, code: number): never {//never return anything
+  throw { message: message, errorCode: code };
+  // while (true) {} //will also work
+}
+generateError('An error occurred!', 500);
