344b002f491b99562815a67a62c74aaf404a3c9d
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-26 09:03:59 +0100

    Type assignment

--- video-9/app.ts
+++ video-9/app.ts
@@ -2,8 +2,9 @@ function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-const number1 = 5;
-const number2 = 2.8;
+let number1: number; //if you just declare a variable without initializing it, it will be of type any
+number1 = 5;
+const number2 = 2.8;//if you initialize a variable, it will be of type number
 
 const result = add(number1, number2);
 console.log(result);
