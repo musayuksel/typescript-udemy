06485f8cf4f3dade1725f0ae65aa786a8e1f7205
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-26 11:52:09 +0100

    Update tuple declaration

--- video-9/objects.ts
+++ video-9/objects.ts
@@ -8,15 +8,20 @@
 // console.log(person.name);
 
 // Better syntax for the above:
-const person2 = {
+const person2:{
+    name: string;
+    age: number;
+    hobbies: string[];
+    roles: [number, string]; // Tuple
+} = {
   name: 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'], // Array of strings
   roles: [2, 'author'], // Tuple** - an array with a fixed number of elements where the types of each element are known ((string | number)[])
 };
 
-person2.roles.push('admin'); // This is allowed
-person2.roles[1] = 10; // This is allowed
+person2.roles.push('admin'); // This is still allowed
+// person2.roles[1] = 10; // This is allowed
 // let favoriteActivities: string[]; // Array of strings
 // favoriteActivities = ['Sports'];
 
