d5f5e239bbe1598ef27f2fc09bcb2af752a6d49b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-26 14:36:55 +0100

    Add callBackFunctions

--- video-9/functionTypes.ts
+++ video-9/functionTypes.ts
@@ -10,8 +10,19 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
-let combineValues: (num1: number, num2: number) => number ;//function get 2 numbers and return a number
+let combineValues: (num1: number, num2: number) => number; //function get 2 numbers and return a number
 combineValues = addNumbers;
 // combineValues = printResult; //error
 
-console.log(combineValues(8, 8));
+// console.log(combineValues(8, 8));
+
+function callbackFunction(result: number) {
+  return result;
+}
+// CALLBACKS
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  //callback function might return any type, we say void because we don't care about the return type
+  const result = n1 + n2;
+  cb(result);
+}
+addAndHandle(10, 20, callbackFunction);//will work even if we declare the callback function as returning void because we don't care about the return type
