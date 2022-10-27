1695fbeed054710b548d07da2d8c8e7365842a3c
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-27 15:49:08 +0100

    Add SINGLETON

--- video-70_SINGLETONS/src/app.ts
+++ video-70_SINGLETONS/src/app.ts
@@ -16,10 +16,22 @@ abstract class Department {
 // I WANT TO CREATE A SINGLETON CLASS SO THAT I CAN NOT CREATE MULTIPLE INSTANCE OF THIS CLASS
 // EXM: company can have only one IT department
 class ITDepartment extends Department {
-  constructor(id: string, private newEmployees: string[]) {
+  //1 - DO PRIVATE CONSTRUCTOR
+  private constructor(id: string,  employee: string[]) {
     super('IT', id);
+    this.employee = employee;
   }
-
+  //2 - CREATE A STATIC PROPERTY
+  private static instance: ITDepartment;
+  //3 - CREATE A STATIC METHOD
+  static getInstance() {
+    // if (this.instance) {
+    if (ITDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new ITDepartment('d1', ['Max']);
+    return this.instance;
+  }
   static myStaticProperty = 'Hello from static property';
 
   sayHello(this: ITDepartment, name2: string): void {
@@ -27,3 +39,12 @@ class ITDepartment extends Department {
     console.log(this.employee);
   }
 }
+
+// const it = new ITDepartment('d1', ['Max']);// ERROR
+const it = ITDepartment.getInstance();
+it.addEmployee('Max2');
+console.log(it)
+
+const it2 = ITDepartment.getInstance();//it and it2 are same instance
+it2.addEmployee('Max3');
+console.log(it2)
