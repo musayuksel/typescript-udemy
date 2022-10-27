23b71f28f9aee4883b73fbdf840f71b0d122815e
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-27 10:49:48 +0100

    Add simple class

--- video-59/src/app.ts
+++ video-59/src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+class Department {
+  name: string; // = 'Accounting';// can assign public by default
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const accounting = new Department('Accounting');
+
+console.log({ accounting });
