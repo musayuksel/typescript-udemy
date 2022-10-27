61910d54d5621c6b55f6bc8698787950f1369c60
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-27 11:21:19 +0100

    Add different methods and test

--- video-59/src/app.ts
+++ video-59/src/app.ts
@@ -1,13 +1,21 @@
 class Department {
   name: string; // = 'Accounting';// can assign public by default
+  employee: string[] = [];
   constructor(name: string) {
     this.name = name;
   }
   sayHello(arg: string = 'hello') {
     console.log(`Hello from ${this.name} department!!! ${arg}`);
   }
+  addEmployee(employee: string) {
+    this.employee.push(employee);
+  }
 }
 
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+console.log({ accounting });
 
-console.log({ accounting }, accounting.sayHello());
+const accounting2 = new Department('Accounting2');
+accounting2.addEmployee('Max2');
+console.log({ accounting2 });
