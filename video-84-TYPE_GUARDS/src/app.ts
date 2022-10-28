37aabee30163e67560e3f4adfccb0ab4b94c4de1
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-28 15:58:28 +0100

    Add initial code for type guard

---
+++ video-84-TYPE_GUARDS/src/app.ts
@@ -0,0 +1,23 @@
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type ElevatedEmployee = Employee & Admin;
+// WILL COMBINE THE TWO TYPES INTO ONE
+const employee1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;// WILL BE NUMBER
+// type Universal2 = Combinable | Numeric;// WILL BE STRING | NUMBER | BOOLEAN
