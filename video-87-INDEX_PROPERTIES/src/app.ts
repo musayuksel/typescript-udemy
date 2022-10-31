ca07df684ef4670e107dcd82e12be7540eeef507
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-31 10:23:53 +0000

    Add INDEX PROPERTIES

--- video-87-INDEX_PROPERTIES/src/app.ts
+++ video-87-INDEX_PROPERTIES/src/app.ts
@@ -1,1 +1,15 @@
 const inputElement = document.getElementById('user-input') as HTMLInputElement; // Type Casting
+interface ErrorContainer {
+  // { email: 'Not a valid email', username: 'Must start with a character!' }
+  [prop: string]: string; // we can use any property name also any number of properties
+}
+
+const errorBagEmpty: ErrorContainer = {}; //we can use empty object also
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a capital character!',
+  // 1: 'test', // we can use any property name also any number of properties
+};
+
+inputElement.value = errorBag.email; // Not a valid email!
