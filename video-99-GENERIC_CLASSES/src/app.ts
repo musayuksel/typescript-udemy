4fdbbf5bec4725e3060320f280ab53b9ba4ed85b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-31 14:36:17 +0000

    Add GENERIC CLASSES

---
+++ video-99-GENERIC_CLASSES/src/app.ts
@@ -0,0 +1,21 @@
+class DataStorage<T> {
+  // data is an array of type T > we will initialize it in the constructor
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+//FLEXIBLE AND STRONG TYPING = GOOD = GENERIC CLASSES
+const textStorage = new DataStorage<string>(); // we can only store STRING in this instance
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>(); // we can only store NUMBER in this instance
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+console.log(numberStorage.getItems());
