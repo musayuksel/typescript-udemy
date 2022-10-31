3fa48913425ac5b5be8b1ee688f65462a2eaee3a
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-31 10:09:21 +0000

    Add TYPE CASTING

--- video-85-DISCRIMINATED_UNIONS/src/app.ts
+++ video-85-DISCRIMINATED_UNIONS/src/app.ts
@@ -4,7 +4,7 @@ interface Bird {
 }
 
 interface Horse {
-  type: 'horse';// Discriminated Union > we can use any property name
+  type: 'horse'; // Discriminated Union > we can use any property name
   runningSpeed: number;
 }
 
@@ -27,3 +27,16 @@ const bird: Bird = {
 };
 moveAnimal(bird);
 moveAnimal({ type: 'horse', runningSpeed: 20 });
+
+// Type Casting>>>>>>
+// Path: video-86-TYPE_CASTING
+// if we add an element to the DOM, <inout type='text' id='user-input'>
+//
+const inputElement = <HTMLInputElement>document.getElementById('user-input');//if we use React, we can use 'as' instead of '<>'
+const inputElement2 = document.getElementById('user-input') as HTMLInputElement; // Type Casting
+inputElement.value = 'Hi there!';
+// if we are not sure the element is null or not we can use if statement
+const inputElement3 = document.getElementById('user-input');
+if (inputElement3) {
+  (inputElement3 as HTMLInputElement).value = 'Hi there!';
+}
